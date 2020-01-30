eec9883a534b7debdc6759081dd45eb082708258
OumarCh <o.haidara@ecole-ipssi.net>
2020-01-30 23:35:30 +0100

    session schema created

---
+++ src/models/sessionModel.js
@@ -0,0 +1,27 @@
+// src/api/models/userModel.js
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+let userSchema = new Schema({
+  nom_session: {
+    type: String,
+    required: true,
+  },
+  promo: {
+    type: String,
+    required: true
+  },
+  annee: {
+    type: Number,
+    required: true
+  },
+  created_at: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+mongoose.model('Session', userSchema);
+
+module.exports = mongoose.model('Session');
