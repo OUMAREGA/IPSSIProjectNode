3935f7603ba097855df63a474f018b10bfad737d
EL IDRISSI RIOUI <ouijdane.elidrissirioui@cgi.com>
2020-01-31 12:23:37 +0100

    mise à jour de routesSession

--- src/routes/routesSession.js
+++ src/routes/routesSession.js
@@ -2,11 +2,11 @@ const sessionController = require('../controllers/sessionController');
 
 // Exporte la fonction anonyme
 module.exports = (app) => {
-  app.route('/posts/:post_id/comments')
-  .get(sessionController.list_all_session_from_a_post)
-  .post(sessionController.create_a_session);
+  app.route('/sessions')
+  .get(sessionController.list_all_sessions)
+  .post(sessionController.session_register);
 
-  app.route('/comments/:comment_id') // req.params.session_id
+  app.route('/sessions/:session_id') // req.params.session_id
   .get(sessionController.get_a_session)
   .put(sessionController.update_a_session)
   .delete(sessionController.delete_a_session);
