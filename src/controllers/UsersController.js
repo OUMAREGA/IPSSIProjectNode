b22efff3026b973c440bb28e23dc0071eb18db1b
Feycal16 <bellili.feycal@gmail.com>
2020-02-02 00:33:29 +0100

    Cryptage du mot de passe + gestion des erreurs du schéma

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,24 +1,36 @@
 const User = require('../models/userModel')
 const jwt = require('jsonwebtoken')
-
+const bcrypt = require("bcrypt") //methode de hash pour crypter le mot de passe
+const robustesse = 10 //plus la valeur élevée plus la méthode de hachage sera lourde pour le mot de passe
 exports.create_user = (req, res) => {
-    let new_user = new User(req.body)
-    try {
-        // Avant de sauvegarder l'utilisateur, on va vérifier si l'adresse mail est conforme
-        new_user.validate().save((error, user) => {
-            if (error) {
+    let errors = [] //pour récupérer les erreurs envoyées depuis la base (définis dans le schéma : required, validate...)
+
+	bcrypt.hash(req.body.password, robustesse, (err,hash) => {
+		req.body.password = hash
+		let new_user = new User(req.body)
+		try{
+			new_user.validate().then(() => new_user.save((error,user) => {
+				if(error){
+                    Object.values(error.errors).forEach((e) => { //on ne récupère que les attributs erronées avec leur message d'erreur
+                         errors[e.path] = e.message
+                    })
+					res.status(400)
+                    res.json(Object.assign({},errors)) //conversion du tableau d'erreur en object js (plus facile à manipuler)
+				}else{
+					res.status(201).json(user)
+				}
+			})).catch((error) => { //passage dans le catch si la validation de l'email échoue
+                Object.values(error.errors).forEach((e) => { //manipulation identique sauf qu'on rajoute ici le message d'échec de validation
+                     errors[e.path] = e.message
+                })
                 res.status(400)
-                res.json({ message: "Il manque des informations" })
-            }
-            else {
-                res.status(201)
-                res.json(user)
-            }
-        })
-    } catch (e) {
-        res.status(500)
-        res.json({ message: "Erreur serveur." })
-    }
+                res.json(Object.assign({},errors))
+            })
+		}catch(e){
+			res.status(500)
+			res.json({message: "Erreur serveur"})
+		}
+	})
 
 }
 
@@ -42,7 +54,7 @@ exports.login_user = (req, res) => {
             } else {
             // si l'utilisateur n'existe pas, on renvoit une erreur 404 
                 res.status(404);
-                res.json({message: "Utilisateur introuvable"})
+                res.json({message: "Utilisateur introuvable" })
             }
 
         })
