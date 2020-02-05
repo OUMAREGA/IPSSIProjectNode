9cba57dfef86bcf886ab04383750447817dff131
YadalleeBilaal <bilaal.yadallee@gmail.com>
2020-02-05 20:25:00 +0100

    [CONTROLLER] Complétion pour le login

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -35,33 +35,48 @@ exports.create_user = (req, res) => {
 }
 
 exports.login_user = (req, res) => {
-    let body = req.body
+    let {body} = req; //syntaxe ES6
     try {
         User.findOne({ email: body.email }, (error, user) => {
+            
             if (user) {
-            // si l'utilisateur existe, on passe par JWT pour générer un token 
-                jwt.sign({ email: user.email }, process.env.JWT_KEY, { expiresIn: "30m" }, (jwtError, token) => {
-                    if (jwtError) {
-                        console.log(jwtError);
-                        res.status(500);
-                        res.json({ message: "Erreur serveur" });
-                    }
-                    else {
-                        res.status(200);
-                        res.json({ token });
+                // si l'utilisateur existe, on passe vérifie d'abord son mot de passe
+                bcrypt.compare(body.password, user.password, (err, resultat) => {
+                    if (!resultat || err) //si la vérification échoue ou qu'il n'a pas saisi le mot de passe
+                    {
+                        res.status(422).json({ message: "mot de passe invalide" })
+                        /**
+                         * On renvoie une erreur 422 (unprocessable entity) car:
+                         * la requête de l'utilisateur est correcte (sinon erreur 400)
+                         * Et il n'a accédé à aucune ressource sans autorisation (sinon erreur 401)
+                         * et sans problème de droits (sinon erreur 403)
+                         */
+                    }else { //si le mot de passe est valide on peut passer par JWT
+                        jwt.sign({ name: user.name, firstname: user.firstname, email: user.email, roleId: user.roleId }, process.ENV.JWT_KEY, { expiresIn: "30m" }, (jwtError, token) => {
+                            if (jwtError) {
+                                console.log(jwtError);
+                                res.status(500);
+                                res.json({ message: "Erreur serveur" });
+                            }
+                            else { //si tout passe on peut renvoyer un token
+                                res.status(200);
+                                res.json({ token });
+                            }
+                        })
                     }
                 })
+
             } else {
-            // si l'utilisateur n'existe pas, on renvoit une erreur 404 
+                // si l'utilisateur n'existe pas, on renvoit une erreur 404 
                 res.status(404);
-                res.json({message: "Utilisateur introuvable" })
+                res.json({ message: "Utilisateur introuvable" })
             }
 
         })
 
     } catch (e) {
         res.status(500)
-        res.json({ message: "Erreur serveur." })
+        res.json({message: "erreur serveur"})
     }
 }
 
