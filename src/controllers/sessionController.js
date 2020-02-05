76b85cc83405e65db6bb22d9e001c7e278fd2b16
OumarCh <o.haidara@ecole-ipssi.net>
2020-02-05 21:05:33 +0100

    optimized return error

--- src/controllers/sessionController.js
+++ src/controllers/sessionController.js
@@ -1,6 +1,7 @@
 // src/api/controllers/userController.js
 const mongoose = require('mongoose'); //Inclusion de l'odm
 const Session = require('../models/sessionModel'); //Inclusion du modèle 
+let errors = [] //pour récupérer les erreurs envoyées depuis la base (définis dans le schéma : required, validate...)
 
 //Créer une session
 exports.session_register = (req, res) => {
@@ -8,14 +9,18 @@ exports.session_register = (req, res) => {
 
   try{
     new_session.save((error, session) => {
-      if(error){
+        if(error){
+          Object.values(error.errors).forEach((e) => { //on ne récupère que les attributs erronées avec leur message d'erreur
+          errors[e.path] = e.message;
+        })
+
         res.status(400);
-        res.json({message: "Il manque des infos"});
+        res.json(Object.assign({},errors)); //conversion du tableau d'erreur en object js (plus facile à manipuler)
       }
       else{
         res.status(201);
         session = session.toObject();
-        res.json(session)
+        res.json(session);
       }
     })
   }
@@ -29,16 +34,18 @@ exports.session_register = (req, res) => {
 exports.list_all_sessions = (req, res) => {
     Session.find({}, (error, sessions) => {
       if(error){
-        res.status(500);
-        console.log(error);
-        res.json({message: "Erreur serveur."})
+        Object.values(error.errors).forEach((e) => { //manipulation identique sauf qu'on rajoute ici le message d'échec de validation
+          errors[e.path] = e.message;
+        })
+        res.status(400);
+        res.json(Object.assign({},errors));
       }
       else if(!sessions){
         res.status(404);
         console.log(error);
       }
       else{
-	res.status(200);
+	      res.status(200);
         res.json(sessions);
       }
     })
@@ -91,9 +98,11 @@ exports.delete_a_session = (req, res) => {
     try {
       Session.findByIdAndRemove(req.params.session_id, (error) => {
         if(session == null){
-          res.status(404);
-          console.log(error);
-          res.json({message: "Id introuvable"});
+          Object.values(error.errors).forEach((e) => { //manipulation identique sauf qu'on rajoute ici le message d'échec de validation
+            errors[e.path] = e.message;
+          })
+          res.status(400);
+          res.json(Object.assign({},errors));
         }
         else{
           res.status(200);
