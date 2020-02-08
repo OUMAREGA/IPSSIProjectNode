00b049bffa21967616be4ff79d81af3dc0a4876f
ii02735 <muhammad.yadallee1@etu.parisdescartes.fr>
2020-02-08 20:58:35 +0100

    [CONTROLLER] Amélioration du controller et factory

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -16,8 +16,16 @@ exports.create_user = (req, res) => {
             make_user.validate().then(() => make_user.save((error, user) => { //sinon on procède d'abord à la validation des données
                 if (!error || user) //à ce stade on peut sauvegarder / tester la sauvegarde
                     res.status(201).json(user);
-                else
-                    res.status(500).json({ message: "Erreur serveur" })
+                else{
+                    if(error.errors)
+                    {
+                        Object.values(error.errors).forEach((e) => { //on récupère les erreurs par attribut erroné
+                            errors[e.path] = e.message
+                        })
+                        res.status(400).json(Object.assign({}, errors)) //Après conversion, et on les affiche dans un objet JSON
+                    }else
+                     res.status(500).json({ message: "Erreur serveur" })
+                }
             }))
                 .catch((err) => { //si la sauvegarde échoue...
 
@@ -34,8 +42,8 @@ exports.create_user = (req, res) => {
 //Authentifier un utilisateur
 exports.login_user = (req, res) => {
     let { body } = req; //syntaxe ES6
-    try {
-        userFactory.instance().findOne({ email: body.email }, (error, user) => {
+
+        userFactory.findOneGen({ email: body.email }).then((user, error) =>{
 
             if (user) {
                 // si l'utilisateur existe, on passe vérifie d'abord son mot de passe
@@ -50,7 +58,7 @@ exports.login_user = (req, res) => {
                          * et sans problème de droits (sinon erreur 403)
                          */
                     } else { //si le mot de passe est valide on peut passer par JWT
-                        jwt.sign({ name: user.name, firstname: user.firstname, email: user.email, roleId: user.roleId }, process.ENV.JWT_KEY, { expiresIn: "30m" }, (jwtError, token) => {
+                        jwt.sign({ name: user.name, firstname: user.firstname, email: user.email, roleId: user.roleId }, process.env.JWT_KEY, { expiresIn: "30m" }, (jwtError, token) => {
                             if (jwtError) {
                                 console.log(jwtError);
                                 res.status(500);
@@ -69,48 +77,44 @@ exports.login_user = (req, res) => {
                 res.json({ message: "Utilisateur introuvable" })
             }
 
-        })
+        }).catch(() => res.status(500).json({ message: "Erreur serveur" }))
+}
 
-    } catch (e) {
-        res.status(500)
-        res.json({ message: "Erreur serveur" })
-    }
+exports.fetch_a_user = (req,res) => {
+
+    userFactory.findOneGen({ _id: req.params.userId }).then((user,error)=>{
+        if(user)
+            res.json(user)
+        else{
+            res.status(404).json({message: "Utilisateur introuvable"})
+        }
+    }).catch((err) => res.status(500).json({ message: "Erreur serveur"}))
 }
 
 /**
  * Chercher des utilisateurs
+ * Possibilité d'utiliser des query params pour
+ * affiner la recherche par le type d'utilisateur
  */
 exports.fetch_users = (req, res) => {
 
     try {
-        if (req.query.role) { //on vérifie qu'une query param existe en URI
-
-            if (["admin", "intervenant", "étudiant"].includes(req.query.role)) { //si cela correspond aux types d'user existants
-                //étant donné qu'on a appliqué un discriminant on utilise le même objet depuis le schéma
-                userFactory.instance(res.query.role).find({ roleId: req.query.role }, (error, users) => {
-                    if (!error) {
-                        if (users.length == 0) //on utilise find et pas findOne donc cela renvoie un tableau
-                            res.status(404).json({ message: `Aucun ${req.query.role} disponible` })
-                        else
-                            res.status(200).json(users)
-                    } else {
-                        console.log(error)
-                        res.status(500).json({ message: "Erreur serveur" })
-                    }
-                })
-            } else {
-                res.status(400).json({ message: "Type d'utilisateur recherché inconnu" })
-            }
+       
+        if (req.query.role && ["admin","étudiant","intervenant"].includes(req.query.role)) { //on vérifie qu'une query param existe en URI
+                userFactory.find(req.query.role,{}).then((users, error) => {
+                    if(users.length != 0)
+                        res.status(200).json(users)
+                    else
+                        res.status(404).json({ message: `Aucun ${req.query.role} disponible` })
+                }).catch((err) => res.status(500).json({ message: "Erreur serveur" }))
+  
         } else {
-            console.log(userFactory.instance())
-            userFactory.instance().find({}, (error, users) => {
-                if (!error) {
+            userFactory.findAll({}).then((users, error) => {
                     if (users.length == 0)
                         res.status(404).json({ message: "Aucun utilisateur disponible" })
                     else
                         res.status(200).json(users)
-                }
-            })
+            }).catch((err) => res.status(500).json({ message: "Erreur serveur" }))
         }
     } catch (e) {
         console.log(e)
@@ -118,39 +122,67 @@ exports.fetch_users = (req, res) => {
     }
 }
 
-//mettre à jour un utilisateur
+
 exports.update_user = (req, res) => {
 
+    let errors = []
     try {
-
+                //pour l'étudiant
         if (req.body.sessionId) //si on a passé une session, on doit vérifier son existance (les middlewares de mongoose ne fonctionnent pas)
         {
-            if( req.body.roleId == "étudiant") {
             const Session = require("../models/sessionModel")
 
             Session.findById(req.body.sessionId, (error, session) => {
                 if (!error || session) {
-                    userFactory.instance().findOneAndUpdate({ _id: req.params.id }, req.body, { new: true, useFindAndModify: true }, (error, updated_user) => {
-                        if (!error || updated_user)
-                            res.status(200).json(updated_user)
-                        else
-                            res.status(500).json({ message: "Erreur serveur" })
-                    })
+                        userFactory.update("étudiant",{ _id: req.params.userId },req.body)
+                        .then((data) => res.status(200).json(data))
+                        .catch((err) => {
+                            if(err.errors){
+                                Object.values(err.errors).forEach((e) => { //on récupère les erreurs par attribut erroné
+                                    errors[e.path] = e.message
+                                })
+                                res.status(400).json(Object.assign({}, errors)) //Après conversion, et on les affiche dans un objet JSON            
+                            }else{
+                                res.status(404).json({ message: "Étudiant introuvable" })
+                            }
+                        })
+     
                 }
-            }).catch((err) => res.status(400).json({ message: "Session invalide"} ))
-            }else{
-                res.status(403).json({ message: "Utilisateur non compatible avec une session" })
-            }
-        } else {
-            userFactory.instance().findOneAndUpdate({ _id: req.params.id }, req.body, { new: true, useFindAndModify: true }, (error, updated_user) => {
-                if (!error || updated_user)
-                    res.status(200).json(updated_user)
-                else
-                    res.status(500).json({ message: "Erreur serveur" })
+            }).catch((err) => res.status(404).json({ message: "Session introuvable"} ))
+        } else if(req.body.password) { //si on veut mettre à jour le mot de passe
+            bcrypt.hash(req.body.password, robustesse).then((hash) => {
+                console.log(hash)
+                req.body.password = hash;
+                userFactory.update({_id: req.params.userId },req.body).then((up)=>{
+                    res.status(200).json(up)
+                }).catch((err) => {
+                    if(err.errors){
+                        Object.values(err.errors).forEach((e) => { //on récupère les erreurs par attribut erroné
+                            errors[e.path] = e.message
+                        })
+                        res.status(400).json(Object.assign({}, errors)) //Après conversion, et on les affiche dans un objet JSON            
+                    }else{
+                        res.status(400).json({ message: "Utilisateur introuvable" })
+                    }
+                })
+            }).catch((err) => res.status(500).json({ message: "Erreur serveur "}))
+        }else{
+            userFactory.update({_id: req.params.userId },req.body).then((up)=>{
+                res.status(200).json(up)
+            }).catch((err) => {
+                if(err.errors){
+                    Object.values(err.errors).forEach((e) => { //on récupère les erreurs par attribut erroné
+                        errors[e.path] = e.message
+                    })
+                    res.status(400).json(Object.assign({}, errors)) //Après conversion, et on les affiche dans un objet JSON            
+                }else{
+                    res.status(400).json({ message: "Utilisateur introuvable" })
+                }
             })
         }
 
     } catch (e) {
+        console.log(e)
         res.status(500).json({ message: "Erreur serveur" })
     }
 }
@@ -158,8 +190,8 @@ exports.update_user = (req, res) => {
 exports.delete_user = (req,res) => {
 
     try{
-        userFactory.instance().findByIdAndRemove(req.params.id,(error) => {
-            if(error)
+        userFactory.delete({ _id: req.params.userId }).then((user) => {
+            if(!user)
                 res.status(400).json({ message: "Utilisateur introuvable"})
             else{
                 res.status(200).json({ message: "Utilisateur supprimé"})
