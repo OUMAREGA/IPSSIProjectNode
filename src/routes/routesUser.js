c1d2473bf1f598aeaf7bd89c426ed6b264e26db0
ii02735 <muhammad.yadallee1@etu.parisdescartes.fr>
2020-02-08 23:29:08 +0100

    Middlewares assignés

--- src/routes/routesUser.js
+++ src/routes/routesUser.js
@@ -1,16 +1,16 @@
 const usersController = require('../controllers/UsersController');
-
+const middleware = require("../middleware/jwtMiddleware")
 module.exports = (app) => {
   app.route('/users')
-  .post(usersController.create_user)
-  .get(usersController.fetch_users)
+  .post([middleware.verify_token,middleware.is_admin],usersController.create_user)
+  .get([middleware.verify_token,middleware.is_admin],usersController.fetch_users)
 
 
   app.route('/users/:userId')
-    .get(usersController.fetch_a_user)
-    .put(usersController.update_user) //pour mettre à jour une session ou son mot de passe, adresse mail, etc.
+    .get([middleware.verify_token,middleware.is_admin],usersController.fetch_a_user)
+    .put([middleware.verify_token,middleware.is_admin],usersController.update_user) //pour mettre à jour une session ou son mot de passe, adresse mail, etc.
 
-  app.delete("/users/:userId",usersController.delete_user)
+  app.delete([middleware.verify_token,middleware.is_admin],"/users/:userId",usersController.delete_user)
 
   app.route('/login')
   .post(usersController.login_user);
