5ded7daf88313d48d0f67ee3518a28606502b047
MAREGA OUMAR <OMarega@MacBook-Pro-de-MAREGA.local>
2020-02-09 21:34:35 +0100

    fix userByTokeb

--- src/controllers/userByTokenController.js
+++ src/controllers/userByTokenController.js
@@ -5,8 +5,9 @@ const jwt = require('jsonwebtoken');
  * GET
  * a user
  */
-exports.getUser = (req, res) => {
-    const usertoken = sessionStorage.getItem('token');
+exports.getUser= (req, res) => {
+    const usertoken = req.body.token;
+    res.json(usertoken)
     if(usertoken){
         const decoded = jwt.verify(usertoken, process.env.JWT_KEY);
         res.json(decoded);
