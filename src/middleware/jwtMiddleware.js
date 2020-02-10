661d9a403894a29f36c7f3e190572397a5b4f011
Yadallee Bilaal <bilaal.yadallee@gmail.com>
2020-02-10 17:31:42 +0100

    [AUTH] Correction de la réponse pour 401

--- src/middleware/jwtMiddleware.js
+++ src/middleware/jwtMiddleware.js
@@ -10,48 +10,45 @@ exports.verify_token = (req, res, next) => {
             if (error) {
                 res.status(403);
                 res.json({ message: "Accès refusé" })
-            }
-            else {
+            } else {
                 res.locals.user = result
-                /**
-                 * //utilisation de res.locals pour créer une variable 
-                 * de portée de la requête en cours, et non à toute l'application (app.locals)
-                 * efficace pour masquer des informations sensibles  
-                 */ 
+                    /**
+                     * //utilisation de res.locals pour créer une variable 
+                     * de portée de la requête en cours, et non à toute l'application (app.locals)
+                     * efficace pour masquer des informations sensibles  
+                     */
                 next(); // next method
             }
         })
+    } else {
+        res.status(401);
+        res.json({ message: "Vous n'êtes pas authentifié" })
     }
+}
+
+exports.is_admin = (req, res, next) => {
+    const { user } = res.locals;
+    if (user.roleId === "admin")
+        next();
     else {
-        res.status(403);
-        res.json({ message: "Accès refusé" })
+        res.status(403).json({ message: "Accès refusé : vous n'êtes pas autorisé à consulter cette ressource" })
     }
 }
 
-exports.is_admin = (req,res,next) => {
-    const {user} = res.locals;
-    if(user.roleId === "admin")
+exports.is_student = (req, res, next) => {
+    const { user } = res.locals;
+    if (user.roleId === "étudiant")
         next();
-    else{
-        res.status(403).json({message: "Accès refusé : vous n'êtes pas autorisé à consulter cette ressource"})
+    else {
+        res.status(403).json({ message: "Accès refusé : vous n'êtes pas étudiant" })
     }
 }
 
-exports.is_student = (req,res,next) => {
-    const {user} = res.locals;
-    if(user.roleId === "étudiant")
+exports.is_teacher = (req, res, next) => {
+    const { user } = res.locals;
+    if (user.roleId === "étudiant")
         next();
-    else{
-        res.status(403).json({message: "Accès refusé : vous n'êtes pas étudiant"})
+    else {
+        res.status(403).json({ message: "Accès refusé : vous n'êtes pas un intervenant" })
     }
 }
-
-exports.is_teacher = (req,res,next) => {
-    const {user} = res.locals;
-    if(user.roleId === "étudiant")
-        next();
-    else{
-        res.status(403).json({message: "Accès refusé : vous n'êtes pas un intervenant"})
-    }
-}
-
