4c3b6691164d93696679b9db6f79fb1754025908
Yadallee Bilaal <bilaal.yadallee@gmail.com>
2020-02-10 17:30:49 +0100

    [SWAGGER] Doc WIP

--- src/app.js
+++ src/app.js
@@ -4,16 +4,16 @@ const bodyParser = require('body-parser');
 const https = require("https") //importation du module https
 const fs = require("fs") //importation de fs pour lire des fichiers
 const app = express();
-
-//const hostname = '0.0.0.0';
+const swaggerUi = require("swagger-ui-express")
+const swaggerDoc = require("./swagger.json")
+    //const hostname = '0.0.0.0';
 const port = 3000;
-
-
 mongoose.connect('mongodb://mongo/insight_BDD');
 
-app.use(bodyParser.urlencoded( {extended: true} ));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.use("/api-doc", swaggerUi.serve, swaggerUi.setup(swaggerDoc))
 
 const moduleRoute = require('./routes/routesModule');
 const userRoute = require("./routes/routesUser")
@@ -30,4 +30,4 @@ userByTokenRoute(app);
 https.createServer({ //configuration HTTPS
     key: fs.readFileSync("https/server.key"), //récupération du fichier server.key (clé de cryptage --> clé PRIVÉE)
     cert: fs.readFileSync("https/server.cert") //récupération du fichier cert (certificat)
-},app).listen(port)
+}, app).listen(port)
