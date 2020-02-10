1a84be537b41d9d69d22accc8b8935cf3c0f2f0c
Yadallee Bilaal <bilaal.yadallee@gmail.com>
2020-02-10 13:46:20 +0100

    [IMPORT] Import utilisateurs + restructuration

--- src/app.js
+++ src/app.js
@@ -4,14 +4,14 @@ const bodyParser = require('body-parser');
 const https = require("https") //importation du module https
 const fs = require("fs") //importation de fs pour lire des fichiers
 const app = express();
-
-//const hostname = '0.0.0.0';
+require("dotenv").config({ path: ".env" }) //importation du fichier .env (sera utilisable par les controllers)
+    //const hostname = '0.0.0.0';
 const port = 3000;
 
 
 mongoose.connect('mongodb://mongo/insight_BDD');
 
-app.use(bodyParser.urlencoded( {extended: true} ));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 
@@ -28,4 +28,4 @@ notesRoute(app);
 https.createServer({ //configuration HTTPS
     key: fs.readFileSync("https/server.key"), //récupération du fichier server.key (clé de cryptage --> clé PRIVÉE)
     cert: fs.readFileSync("https/server.cert") //récupération du fichier cert (certificat)
-},app).listen(port)
+}, app).listen(port)
--- src/import/import.js
+++ src/import/import.js
@@ -1,33 +1,45 @@
 var mongoose = require('mongoose');
-
 // make a connection
-mongoose.connect('mongodb://mongo/insight_BDD',{ useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb://mongo/insight_BDD', { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.set('useCreateIndex', true)
-const users = require("../data/users.json")
+const data = require("../data/users.json")
 const color = require("cli-color")
 const User = require("../models/userModel")
 const bcrypt = require("bcrypt")
-for(user of users)
-{
-    user.password = bcrypt.hashSync(user.password,10)
-    if(user.roleId == "étudiant")
-    {
-        user.sessionId = ""
-    }
+    //on remplace les mots de passe en clair par des chiffrés (nécessaire pour la connexion, car on vérifie les hash)
+for (key of Object.keys(data)) {
+    for (user of data[key])
+        user.password = bcrypt.hashSync(user.password, 10)
 }
 
-User.insertMany(users).then((docs) => { console.log("Utilisateurs importés"); process.exit() })
-    .catch((err) => {
-        let errors = []
-        if (err.errors) {
+//On insert les utilisateurs (chaque type à la suite, car la structure des modèles / schémas peut différer)
+//on commence par les administrateurs
+User.insertMany(data.admins).then((docs) => {
+        console.log(color.green(`${docs.length > 1 ? color.bold(docs.length) + ' administrateurs importés' : 'Un seul administrateur importé'}`));
+    }) //Puis par les intervenants
+    .then(() => User.insertMany(data.intervenants)
+        .then((docs) => {
+            console.log(color.green(`${docs.length > 1 ? color.bold(docs.length) + ' intervenants importés' : 'Un seul intervenant importé'}`));
+        })
+        .then(() => User.Student.insertMany(data.etudiants) //l'étudiant a une structure différente, on invoque User.student
+            .then((docs) => {
+                console.log(color.green(`${docs.length > 1 ? color.bold(docs.length) + ' étudiants importés' : 'Un seul étudiant importé'}`))
+                process.exit()
+            }))
+        .catch((err) => { //s'il y a des erreurs de validation
+            let errors = []
+            if (err.errors) {
 
-            Object.values(err.errors).forEach((e) => { //on récupère les erreurs par attribut erroné
-                errors[e.path] = e.message
-                console.log("Problème pour : " + e.value)
-                console.log(e.path+": "+e.message)
-            })
-        }else{
-            console.log(err)
-        }
-        process.exit()
+                Object.values(err.errors).forEach((e) => { //on récupère les erreurs par attribut erroné
+                    errors[e.path] = e.message
+                    console.log(color.mangenta("Problème pour : " + e.value))
+                    console.log(color.red(color.bold(e.path) + ": " + e.message))
+                })
+            } else {
+                console.log(color.red(err))
+            }
+            process.exit()
+        })).catch((err) => {
+        console.log(color.red(err.errmsg)) //s'il y a d'autres erreurs (unicité)
+        process.exit() //on met fin au processus pour terminer la promesse (éviter un blocage asynchrone)
     })
